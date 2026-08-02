6c00117d4b880e40cbf3dcf66c1d95c780a0ab0a
xanderswillii <chedjouwilliam465@gmail.com>
2026-08-02 16:05:50 +0100

    implemented the enemey's movement through script and with the animation player

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -6,33 +6,50 @@ enum States {attack, idle, chase, die}
 var state = States.idle
 var hp = 15
 var speed = 2
+var accel = 10
+var gravity = 9.8
+var target = null
 
+@export var navAgent : NavigationAgent3D
+@export var animationPlayer : AnimationPlayer 
 
-func _process(delta):
-	behaviors()
-
-
-func behaviors():
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity
+	
 	if state == States.idle:
 		print("idle")
 		velocity = Vector3.ZERO
+		animationPlayer.play("Idle")
 	elif state == States.chase:
-		print("chase")
-		velocity == Vector3.ZERO
+		look_at(Vector3(target.global_position.x, global_position.y,target.global_position.z), Vector3.UP, true)
+		navAgent.target_position = target.global_position
+		
+		var direction = navAgent.get_next_path_position() - global_position
+		direction = direction.normalized()
+		
+		velocity = velocity.lerp(direction * speed, accel * delta)
+		animationPlayer.play("Walk")
+		
 	elif state == States.attack:
-		print("attack")
+		look_at(Vector3(target.global_position.x, global_position.y,target.global_position.z), Vector3.UP, true)
+		animationPlayer.play("Punch")
 		velocity = Vector3.ZERO
 	elif state == States.die:
 		velocity = Vector3.ZERO
-
+		animationPlayer.play("Die")
+		
+	move_and_slide()
 
 func _on_chase_area_body_entered(body: Node3D) -> void:
 	if body.has_method("player"):
+		target = body
 		state = States.chase
 
 
 func _on_chase_area_body_exited(body: Node3D) -> void:
 	if body.has_method("player"):
+		target = null
 		state = States.idle
 
 
